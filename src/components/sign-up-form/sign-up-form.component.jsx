2c77ceaf0cce1a11ed183fb8ed848748547bc4cf
den-kar <denis.karaula@gmx.de>
2023-02-21 17:02:34 +0100

    created sign up, form input and button  component

---
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -0,0 +1,122 @@
+import { useState } from 'react';
+
+import FormInput from '../form-input/form-input.component';
+import Button from '../button/button.component';
+
+import {
+  createAuthUserWithEmailAndPassword,
+  createUserDocumentFromAuth,
+} from '../../utils/firebase/firebase.utils';
+
+import './sign-up-form.styles.scss';
+
+const defaultFormFields = {
+  displayName: '',
+  email: '',
+  password: '',
+  confirmPassword: '',
+};
+
+const SignUpForm = () => {
+  const [formField, setFormFields] = useState(defaultFormFields);
+  const { displayName, email, password, confirmPassword } = formField;
+
+  // console.log(formField);
+
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormFields({ ...formField, [name]: value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    // const { value } = event.target;
+    // console.log(formField);
+    if (password !== confirmPassword) {
+      alert("password and password confirmation don't match...");
+      return;
+    }
+    try {
+      const { user } = await createAuthUserWithEmailAndPassword(
+        email,
+        password
+      );
+      await createUserDocumentFromAuth(user, { displayName });
+      resetFormFields();
+    } catch (error) {
+      if (error.code === 'auth/email-already-in-use') {
+        alert('Cannot create user, email already in use');
+      } else {
+        alert('user creation encountered an error', error);
+      }
+    }
+    // const userDocRef = logEmailUser();
+    // console.log(userDocRef);
+  };
+
+  // const logEmailUser = async () => {
+  //   const { user } = await signInWithCustomEmailAndPassword(email, password);
+  //   const userDocRef = await createUserDocumentFromAuth(user);
+  //   return userDocRef;
+  // };
+
+  return (
+    <div className='sign-up-container'>
+      <h2>Don't have an account?</h2>
+      <span>Sign up with your email and password</span>
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          label="Display Name"
+          name="displayName"
+          onChange={handleChange}
+          value={displayName}
+          type="text"
+          required
+          // otherProps={{
+          //   name: "displayName",
+          //   onChange: handleChange,
+          //   value: displayName,
+          //   type: "text",
+          //   required: true,
+          // }}
+        />
+
+        <FormInput
+          label="Email"
+          name="email"
+          onChange={handleChange}
+          value={email}
+          type="email"
+          required
+        />
+
+        <FormInput
+          label="Password"
+          name="password"
+          onChange={handleChange}
+          value={password}
+          type="password"
+          required
+        />
+
+        <FormInput
+          label="Confirm Password"
+          name="confirmPassword"
+          onChange={handleChange}
+          value={confirmPassword}
+          type="password"
+          required
+        />
+
+        <Button buttonType='inverted' type="submit">Sign Up</Button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUpForm;
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -2,9 +2,11 @@
 import { initializeApp } from 'firebase/app';
 import {
   getAuth,
-  signInWithRedirect,
+  // signInWithRedirect,
   signInWithPopup,
   GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
 } from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
@@ -21,17 +23,29 @@ const firebaseConfig = {
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
-const provider = new GoogleAuthProvider();
-provider.setCustomParameters({
+const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({
   prompt: 'select_account',
 });
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+
+// export const signInWithGoogleRedirect = () =>
+//   signInWithRedirect(auth, googleProvider);
+
+export const signInWithCustomEmailAndPassword = (email, password) =>
+  signInWithEmailAndPassword(auth, email, password);
 
 export const db = getFirestore();
 
-export const createUserDocumentFromAuth = async (userAuth) => {
+export const createUserDocumentFromAuth = async (
+  userAuth,
+  additionalInformation = {},
+) => {
+  if (!userAuth) return;
+
   const userDocRef = doc(db, 'users', userAuth.uid);
 
   // console.log(userDocRef);
@@ -49,6 +63,7 @@ export const createUserDocumentFromAuth = async (userAuth) => {
         displayName,
         email,
         createdAt,
+        ...additionalInformation,
       });
     } catch (error) {
       console.log('error creating the user', error.message);
@@ -57,3 +72,11 @@ export const createUserDocumentFromAuth = async (userAuth) => {
 
   return userDocRef;
 };
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+
+  return createUserWithEmailAndPassword(auth, email, password);
+};
+
+// export const signInWith
