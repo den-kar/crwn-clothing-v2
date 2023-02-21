9d2655b51146ba00504e8775b800245057af7ed9
den-kar <denis.karaula@gmx.de>
2023-02-21 09:59:44 +0100

    added sign in component as well as firebase utils and libraries

--- src/App.js
+++ src/App.js
@@ -1,102 +1,135 @@
-// import CategoryItem from "./components/category-item/category-item.components";
-import Directory from "./components/directory/directory.component";
+import { Routes, Route } from 'react-router-dom';
 
-const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: 'hats',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-    },
-    {
-      id: 2,
-      title: 'jackets',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-    },
-    {
-      id: 3,
-      title: 'sneakers',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-    },
-    {
-      id: 4,
-      title: 'womens',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-    },
-    {
-      id: 5,
-      title: 'mens',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-    },
-    // { id: 1, title: "Hats" },
-    // { id: 2, title: "Jackets" },
-    // { id: 3, title: "Sneakers" },
-    // { id: 4, title: "Womens" },
-    // { id: 5, title: "Mens" },
-  ];
+import Home from './routes/home/home.component';
+import SignIn from './routes/sign-in/sign-in.component';
+import Navigation from './routes/navigation/navigation.component';
 
+// const Navigation = () => {
+//   return (
+//     <div>
+//       <div>
+//         <h1>I am the navigation bar</h1>
+//       </div>
+//       <Outlet />
+//     </div>
+//   );
+// };
+
+const Shop = () => {
   return (
-    <div>
-      <Directory categories={categories} />
-    </div>
-
-    // <div className="categories-container">
-    //   {categories.map((category) => (
-    //     <CategoryItem category={category} key={category.id} />
-    //   ))}
-
-      // {/* {categories.map(({ title, id, imageUrl }) => (
-      //   <div className="category-container" key={id}>
-      //     <div
-      //       className="background-image"
-      //       style={{
-      //         backgroundImage: `url(${imageUrl})`,
-      //       }}
-      //     ></div>
-      //     <div className="category-body-container">
-      //       <h2>{title}</h2>
-      //       <p>Shop now</p>
-      //     </div>
-      //   </div>
-      // ))} */}
-
-    //   {/* <div className="category-container">
-    //     <img />
-    //     <div className="category-body-container">
-    //       <h2>Hats</h2>
-    //       <p>Shop now</p>
-    //     </div>
-    //   </div>
-    //   <div className="category-container">
-    //     <img />
-    //     <div className="category-body-container">
-    //       <h2>Jackets</h2>
-    //       <p>Shop now</p>
-    //     </div>
-    //   </div>
-    //   <div className="category-container">
-    //     <img />
-    //     <div className="category-body-container">
-    //       <h2>Sneakers</h2>
-    //       <p>Shop now</p>
-    //     </div>
-    //   </div>
-    //   <div className="category-container">
-    //     <img />
-    //     <div className="category-body-container">
-    //       <h2>Womens</h2>
-    //       <p>Shop now</p>
-    //     </div>
-    //   </div>
-    //   <div className="category-container">
-    //     <img />
-    //     <div className="category-body-container">
-    //       <h2>Mens</h2>
-    //       <p>Shop now</p>
-    //     </div>
-    //   </div>
-    // </div> */}
+    <h1>I am the shop page</h1>
   );
 };
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
+      </Route>
+    </Routes>
+  );
+};
+
+// import CategoryItem from "./components/category-item/category-item.components";
+// import Directory from "./components/directory/directory.component";
+
+// const App = () => {
+//   const categories = [
+//     {
+//       id: 1,
+//       title: 'hats',
+//       imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+//     },
+//     {
+//       id: 2,
+//       title: 'jackets',
+//       imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+//     },
+//     {
+//       id: 3,
+//       title: 'sneakers',
+//       imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+//     },
+//     {
+//       id: 4,
+//       title: 'womens',
+//       imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+//     },
+//     {
+//       id: 5,
+//       title: 'mens',
+//       imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+//     },
+//     // { id: 1, title: "Hats" },
+//     // { id: 2, title: "Jackets" },
+//     // { id: 3, title: "Sneakers" },
+//     // { id: 4, title: "Womens" },
+//     // { id: 5, title: "Mens" },
+//   ];
+
+//   return <Directory categories={categories} />;
+
+//   // return (
+//     // <div className="categories-container">
+//     //   {categories.map((category) => (
+//     //     <CategoryItem category={category} key={category.id} />
+//     //   ))}
+
+//       // {/* {categories.map(({ title, id, imageUrl }) => (
+//       //   <div className="category-container" key={id}>
+//       //     <div
+//       //       className="background-image"
+//       //       style={{
+//       //         backgroundImage: `url(${imageUrl})`,
+//       //       }}
+//       //     ></div>
+//       //     <div className="category-body-container">
+//       //       <h2>{title}</h2>
+//       //       <p>Shop now</p>
+//       //     </div>
+//       //   </div>
+//       // ))} */}
+
+//     //   {/* <div className="category-container">
+//     //     <img />
+//     //     <div className="category-body-container">
+//     //       <h2>Hats</h2>
+//     //       <p>Shop now</p>
+//     //     </div>
+//     //   </div>
+//     //   <div className="category-container">
+//     //     <img />
+//     //     <div className="category-body-container">
+//     //       <h2>Jackets</h2>
+//     //       <p>Shop now</p>
+//     //     </div>
+//     //   </div>
+//     //   <div className="category-container">
+//     //     <img />
+//     //     <div className="category-body-container">
+//     //       <h2>Sneakers</h2>
+//     //       <p>Shop now</p>
+//     //     </div>
+//     //   </div>
+//     //   <div className="category-container">
+//     //     <img />
+//     //     <div className="category-body-container">
+//     //       <h2>Womens</h2>
+//     //       <p>Shop now</p>
+//     //     </div>
+//     //   </div>
+//     //   <div className="category-container">
+//     //     <img />
+//     //     <div className="category-body-container">
+//     //       <h2>Mens</h2>
+//     //       <p>Shop now</p>
+//     //     </div>
+//     //   </div>
+//     // </div> */}
+//   // );
+// };
 
 export default App;
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,59 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCvf-Rwyr8DhNpzhU0dXaWIVB7N-aFSMa8',
+  authDomain: 'crwn-clothing-db-5bb72.firebaseapp.com',
+  projectId: 'crwn-clothing-db-5bb72',
+  storageBucket: 'crwn-clothing-db-5bb72.appspot.com',
+  messagingSenderId: '673416136163',
+  appId: '1:673416136163:web:8fbcbc802fe453ee814cd6',
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: 'select_account',
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  // console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  // console.log(userSnapshot);
+  // console.log(userSnapshot.exists());
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log('error creating the user', error.message);
+    }
+  }
+
+  return userDocRef;
+};
